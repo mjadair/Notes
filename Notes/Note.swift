c76f7e57d05e5fc1b778f91dddc612a9470d0f69
Michael Adair <mjadair91@gmail.com>
2020-07-20 16:15:45 +0100

    instantiated table view

---
+++ Notes/Note.swift
@@ -0,0 +1,15 @@
+//
+//  Note.swift
+//  Notes
+//
+//  Created by Michael Adair on 20/07/2020.
+//  Copyright © 2020 Michael Adair. All rights reserved.
+//
+
+import Foundation
+
+
+struct Note {
+    let id: Int
+    let contents: String
+}
--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -8,12 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var notes: [Note] = []
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    // We need three methods implemented when intialising a table view controller:
+    
+    
+    // 1: Number of Sections
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    // 2: The Number of rows
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // this is the size of our notes array
+        return notes.count
+    }
+    
+    
+    // 3: Cell for row index path
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell", for: indexPath)
+        cell.textLabel?.text = notes[indexPath.row].contents
+        return cell
+    }
+      
+    
+    
 
 
 }
