543abb92c4d8e9349359b0a75b3f59ad86b5a1d0
Michael Adair <mjadair91@gmail.com>
2020-07-20 15:36:00 +0100

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Michael Adair on 20/07/2020.
+//  Copyright © 2020 Michael Adair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
