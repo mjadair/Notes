d354ab7ca88dc0becd51e0be450bbb6c5b9f2bfa
Michael Adair <mjadair91@gmail.com>
2020-07-22 10:38:20 +0100

    further comments

--- Notes/NoteViewController.swift
+++ Notes/NoteViewController.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+
+// this is the controller for our single note views
 class NoteViewController: UIViewController {
     @IBOutlet var contentTextView: UITextView!
     
@@ -7,13 +9,11 @@ class NoteViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         contentTextView.text = note!.content
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         note!.content = contentTextView.text
         NoteManager.shared.saveNote(note: note!)
     }
